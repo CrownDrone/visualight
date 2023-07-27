1efa211f7b1df1ddbe42de7402818c1834983329
ZSTmonarch <visitacionrannel05@gmail.com>
2023-07-28 01:17:44 +0800

    Spacing lang to/ Bonk

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -3,18 +3,19 @@
 $this->title = '';
 ?>
 
-<style> 
+<style>
     @font-face {
-            font-family: 'Poppins';
-            src: url('<?= Yii::$app->request->baseUrl ?>/fonts/Poppins-Light.ttf') format('truetype'),
-                 url('<?= Yii::$app->request->baseUrl ?>/fonts/Poppins-Light.woff') format('woff');
-    
-        }
+        font-family: 'Poppins';
+        src: url('<?= Yii::$app->request->baseUrl ?>/fonts/Poppins-Light.ttf') format('truetype'),
+            url('<?= Yii::$app->request->baseUrl ?>/fonts/Poppins-Light.woff') format('woff');
+
+    }
+
     :root {
         font-size: 16px;
     }
-    .header
-    {
+
+    .header {
         color: #0362BA;
         font-family: Poppins;
         font-size: 2rem;
@@ -26,17 +27,16 @@ $this->title = '';
         justify-content: space-between;
         align-items: center;
     }
-    .header img
-    {
+
+    .header img {
         float: right;
         height: 3rem;
         margin-bottom: 1rem;
     }
-    
-    .DailyTransaction
-    {
+
+    .DailyTransaction {
         width: 100%;
-        height: 10.8125rem; 
+        height: 10.8125rem;
         border-radius: .635rem;
         background: #EFF5FF;
         text-align: center;
@@ -47,12 +47,12 @@ $this->title = '';
         font-weight: 600;
         line-height: normal;
         letter-spacing: 2.4px;
-        
 
-        
+
+
     }
-    .deptransaction
-    {
+
+    .deptransaction {
         width: 32%;
         height: 7.875rem;
         border-radius: .635rem;
@@ -64,118 +64,107 @@ $this->title = '';
         font-weight: 700;
         line-height: normal;
         letter-spacing: 2.4px;
-        display:inline-block;
+        display: inline-block;
     }
-    .deptransaction img
-    {
+
+    .deptransaction img {
         float: left;
         margin-left: .625rem;
     }
-    #valueIncrease
-    {
+
+    #valueIncrease {
         font-size: 1.5rem;
         font-weight: 400;
         letter-spacing: 3.6px;
-        grid-column: 3; /* Position this div in the second column */
-        text-align: right; /* Align the text to the right (bottom right) */
+        grid-column: 3;
+        /* Position this div in the second column */
+        text-align: right;
+        /* Align the text to the right (bottom right) */
         padding-top: 40px;
-       
+
     }
-    #dailyTrans
-    {
+
+    #dailyTrans {
         font-size: 3.375rem;
         font-style: normal;
         font-weight: 700;
         line-height: normal;
         letter-spacing: 8.1px;
     }
+
     .grid {
         display: grid;
-        grid-template-columns: repeat(3, 1fr); 
+        grid-template-columns: repeat(3, 1fr);
         grid-gap: .125rem;
         grid-template-rows: auto auto;
-        
+
     }
 
     /* responsiveness */
 
     @media (max-width: 900px) {
-            .deptransaction {
-                width: 80%;
-                display: justify; /* Change to block to stack vertically */
-                margin: 0 auto 1rem;
-                
-                
-            }
-            .header
-            { 
-                font-size: 1rem;
-            }
-            .header img
-            {
-                height: 2rem;
-                margin-bottom: 1rem;
-            }
+        .deptransaction {
+            width: 80%;
+            display: justify;
+            /* Change to block to stack vertically */
+            margin: 0 auto 1rem;
+
+
         }
-    
 
+        .header {
+            font-size: 1rem;
+        }
 
-
+        .header img {
+            height: 2rem;
+            margin-bottom: 1rem;
+        }
+    }
 </style>
-<?= 
-    $m="";
-    // metrology transaction
-    $lastmettrans=5;
-    $todaymettrans=3;
-    $metdailytransincrease = (($todaymettrans - $lastmettrans) /$todaymettrans) * 100;
-    $metdailytransincrease = number_format($metdailytransincrease, 2);
-    if($metdailytransincrease>1)
-    {
-        $metdailytransincrease = '+'.$metdailytransincrease . '%';
+<?=
+$m = "";
+// metrology transaction
+$lastmettrans = 5;
+$todaymettrans = 3;
+$metdailytransincrease = (($todaymettrans - $lastmettrans) / $todaymettrans) * 100;
+$metdailytransincrease = number_format($metdailytransincrease, 2);
+if ($metdailytransincrease > 1) {
+    $metdailytransincrease = '+' . $metdailytransincrease . '%';
+} else {
+    $metdailytransincrease = $metdailytransincrease . '%';
+}
 
-        
-    }
-    else
-    {
-        $metdailytransincrease = $metdailytransincrease . '%';
-    }
-
-    //S&T transaction
-    $lastSandTtrans=2;
-    $todaySandTtrans=10;
-    $SandTdailytransincrease = (($todaySandTtrans - $lastSandTtrans) / $todaySandTtrans) * 100;
-    $SandTdailytransincrease = number_format( $SandTdailytransincrease, 2);
-    if($SandTdailytransincrease>1)
-    {
-        $SandTdailytransincrease = '+'.$SandTdailytransincrease . '%';
-    }
-    else
-    {
-        $metdailytransincrease = $metdailytransincrease . '%';
-    }
-    //T&S transaction
-    $lastTandStrans=6;
-    $todayTandStrans=100;
-    $TandSdailytransincrease = (($todayTandStrans - $lastTandStrans) / $todayTandStrans) * 100;
-    $TandSdailytransincrease = number_format( $TandSdailytransincrease, 2);
-    if($TandSdailytransincrease>1)
-    {
-        $TandSdailytransincrease = '+'.$TandSdailytransincrease . '%';
-    }
-    else
-    {
-        $metdailytransincrease = $metdailytransincrease . '%';
-    }
+//S&T transaction
+$lastSandTtrans = 2;
+$todaySandTtrans = 10;
+$SandTdailytransincrease = (($todaySandTtrans - $lastSandTtrans) / $todaySandTtrans) * 100;
+$SandTdailytransincrease = number_format($SandTdailytransincrease, 2);
+if ($SandTdailytransincrease > 1) {
+    $SandTdailytransincrease = '+' . $SandTdailytransincrease . '%';
+} else {
+    $metdailytransincrease = $metdailytransincrease . '%';
+}
+//T&S transaction
+$lastTandStrans = 6;
+$todayTandStrans = 100;
+$TandSdailytransincrease = (($todayTandStrans - $lastTandStrans) / $todayTandStrans) * 100;
+$TandSdailytransincrease = number_format($TandSdailytransincrease, 2);
+if ($TandSdailytransincrease > 1) {
+    $TandSdailytransincrease = '+' . $TandSdailytransincrease . '%';
+} else {
+    $metdailytransincrease = $metdailytransincrease . '%';
+}
 
 
-    ?>
+?>
 
-    <div class="header">
-        <!-- <div class="header-grid"> -->
-            <p>Dashboard</p>
-            <img src="/images/LogoVL.png" alt="visLogo" >
-        <!-- </div> -->
-    </div> <br>
+<div class="header">
+    <!-- <div class="header-grid"> -->
+    <p>Dashboard</p>
+    <img src="/images/LogoVL.png" alt="visLogo">
+    <!-- </div> -->
+</div> <br>
 
 <div class="DailyTransaction">
     <p>Total Transactions Daily</p>
@@ -183,25 +172,25 @@ $this->title = '';
     <div class="deptransaction">
         <p>National Metrology</p>
         <div class="grid">
-        <img src="/images/Pressure Gauge.png" alt="icon1">
-        <p id="dailyTrans"><?=$todaymettrans?></p>
-        <p id="valueIncrease"><?= $metdailytransincrease?></p>
+            <img src="/images/Pressure Gauge.png" alt="icon1">
+            <p id="dailyTrans"><?= $todaymettrans ?></p>
+            <p id="valueIncrease"><?= $metdailytransincrease ?></p>
         </div>
     </div>
     <div class="deptransaction" style="background-color:#02A560;">
         <p>Standards and Testing</p>
         <div class="grid">
-        <img src="/images/Pass Fail.png" alt="icon2">
-        <p id="dailyTrans"><?=$todaySandTtrans?></p>
-        <p id="valueIncrease"><?= $SandTdailytransincrease?></p>
+            <img src="/images/Pass Fail.png" alt="icon2">
+            <p id="dailyTrans"><?= $todaySandTtrans ?></p>
+            <p id="valueIncrease"><?= $SandTdailytransincrease ?></p>
         </div>
     </div>
     <div class="deptransaction" style="background-color:#F21A9C;">
         <p>Technological Services</p>
         <div class="grid">
-        <img src="/images/Service.png" alt="icon3">
-        <p id="dailyTrans"><?=$todayTandStrans?></p>
-        <p id="valueIncrease"><?= $TandSdailytransincrease?></p>
+            <img src="/images/Service.png" alt="icon3">
+            <p id="dailyTrans"><?= $todayTandStrans ?></p>
+            <p id="valueIncrease"><?= $TandSdailytransincrease ?></p>
         </div>
     </div>
 
@@ -248,7 +237,7 @@ $this->title = '';
             <?= \hail812\adminlte\widgets\InfoBox::widget([
                 'text' => 'Bookmarks',
                 'number' => '410',
-                 'theme' => 'success',
+                'theme' => 'success',
                 'icon' => 'far fa-flag',
             ]) ?>
         </div>
@@ -286,7 +275,7 @@ $this->title = '';
                 ]
             ]) ?>
             <?= \hail812\adminlte\widgets\Ribbon::widget([
-                'id' => $infoBox->id.'-ribbon',
+                'id' => $infoBox->id . '-ribbon',
                 'text' => 'Ribbon',
             ]) ?>
             <?php \hail812\adminlte\widgets\InfoBox::end() ?>
@@ -322,7 +311,7 @@ $this->title = '';
                 'theme' => 'success'
             ]) ?>
             <?= \hail812\adminlte\widgets\Ribbon::widget([
-                'id' => $smallBox->id.'-ribbon',
+                'id' => $smallBox->id . '-ribbon',
                 'text' => 'Ribbon',
                 'theme' => 'warning',
                 'size' => 'lg',
