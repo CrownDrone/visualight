4b2a9838810fba32f16f1b696a4e78acc7ccf81d
ZSTmonarch <visitacionrannel05@gmail.com>
2023-08-05 10:49:40 +0800

    Update view.php

--- backend/views/user-profile/view.php
+++ backend/views/user-profile/view.php
@@ -13,7 +13,8 @@ $defaultImagePath = Yii::getAlias('@web') . '/images/user2.jpg';
 <div class="user-profile-view">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= Html::a('Edit Profile', ['update'], ['class' => 'btn btn-primary', 'style' => 'position:absolute; right: 50px; top: 740px; font-weight: 600;']) ?>
+    <?= Html::a('Edit Profile', ['update'], ['class' => 'btn btn-primary', 'style' => 'position:absolute; right: 20px; top: 745px; font-weight: 600;']) ?>
+
     <div style="background-image: url('/images/back_pic.png'); background-repeat: no-repeat; background-size: cover; width: 100%; height: 350px;">
 
         <div style="position: relative; top: 13em; text-align: center; ">
@@ -28,6 +29,9 @@ $defaultImagePath = Yii::getAlias('@web') . '/images/user2.jpg';
     </div>
 
     <table class="table table-bordered" style="overflow-x:auto; border-collapse: collapse;border-style:hidden;">
+        <td>
+
+        </td>
         <tr>
             <td> <strong>Full Name: </strong><?= Html::encode($user->fullName) ?></td>
         </tr>
@@ -52,10 +56,10 @@ $defaultImagePath = Yii::getAlias('@web') . '/images/user2.jpg';
         </tr>
         <tr>
             <td> <strong>Contact Number: </strong>
-            <?= Html::encode($user->contactNumber) ?></td>
+                <?= Html::encode($user->contactNumber) ?></td>
         </tr>
         <tr>
-           <td><strong>Address: </strong><?= Html::encode($user->address) ?></td>
+            <td><strong>Address: </strong><?= Html::encode($user->address) ?></td>
         </tr>
         <tr>
             <!-- <th>Profile Picture:</th> -->
@@ -66,7 +70,7 @@ $defaultImagePath = Yii::getAlias('@web') . '/images/user2.jpg';
             <!-- Display the custom default profile picture if no image is available -->
             <!-- <?= Html::img($defaultImagePath, ['class' => 'img-circle elevation-2', 'style' => 'height: 200px; width: 200px']) ?>
                 <?php endif; ?> -->
-            <!-- </td> -->
-        </tr>
+            </td>
+            <!-- </tr> -->
     </table>
 </div>
