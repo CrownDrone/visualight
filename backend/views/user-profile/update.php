f7a1d64a89ac202c7a2a8d52dec4ee3937950338
ZSTmonarch <visitacionrannel05@gmail.com>
2023-08-06 19:48:49 +0800

    Small Update/ Edit profile
    sadly

--- backend/views/user-profile/update.php
+++ backend/views/user-profile/update.php
@@ -13,45 +13,55 @@ $this->title = '';
 // $this->params['breadcrumbs'][] = $this->title;
 ?>
 
+
+<h4 style="color: #0362BA; font-family: Poppins; font-size: 24px; font-style: normal; font-weight: 600; line-height: normal; letter-spacing: 2.5px;">Edit Profile:</h4>
+
 <div class="user-profile-update">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
-
-    <?= $form->field($model, 'username')->textInput() ?>
-
-    <?= $form->field($model, 'email')->textInput() ?>
-
-    <?= $form->field($model, 'contactNumber')->textInput(['maxlength' => true]) ?>
-
-    <?php $hasPasswordError = $model->hasErrors('existingPassword'); ?>
-    <?= $form->field($model, 'existingPassword')->passwordInput()->hint($hasPasswordError ? '' : '   *Leave it blank if you dont want to change the password', ['style' => 'color: green']) ?>
-
-    <?php $hasPasswordError = $model->hasErrors('newPassword'); ?>
-    <?= $form->field($model, 'newPassword')->passwordInput()->hint($hasPasswordError ? '' : '   *Leave it blank if you dont want to change the password', ['style' => 'color: green']) ?>
-
-    <?= $form->field($model, 'imageFile')->fileInput(['accept' => 'image/*']) // Add the image file input field 
-    ?>
-
-    <!-- Show the current image preview -->
-    <?php if ($model->profile_picture) : ?>
-        <h3>Current Profile Picture:</h3>
-        <?php
-        $profilePicturePath = Url::to(['/user-profile/get-profile-picture', 'fileName' => $model->profile_picture]);
-        echo Html::img($profilePicturePath, ['class' => 'img-circle elevation-2', 'style' => 'height: 300px; width: 300px;', 'id' => 'current-image']); ?>
-    <?php endif; ?>
-    <br>
-    <!-- Add an empty image tag for real-time preview -->
-    <img id="image-preview" src="#" alt="Image Preview" class="img-circle elevation-2" style="height: 300px; width: 300px; display: none; margin-bottom: 10px">
-
-
-    <div class="form-group">
+    <div style="border-radius: 10px; background: #77bfc7; width: 1479px; height: 670px; flex-shrink: 0;">
+        <!-- Left column start  -->
+        <!-- Show the current image preview -->
+        <?php if ($model->profile_picture) : ?>
+            <?php
+            $profilePicturePath = Url::to(['/user-profile/get-profile-picture', 'fileName' => $model->profile_picture]);
+            echo Html::img($profilePicturePath, ['class' => 'img-circle elevation-2', 'style' => 'height: 227px; width: 227px; margin-top: 14%; margin-left: 10%', 'id' => 'current-image']); ?>
+        <?php endif; ?>
         <br>
-        <?= Html::submitButton('Update', ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Back', ['view'], ['class' => 'btn btn-warning']) ?>
+        <!-- Add an empty image tag for real-time preview -->
+        <img id="image-preview" src="#" alt="Image Preview" class="img-circle elevation-2" style="height: 227px; width: 227px; display: none; margin-top: 13%; margin-left: 10% ">
 
+        <!-- End of left column  -->
+
+        <!-- right column  -->
+        <div class="form-group" style="width: 30%; position: absolute; top: 40%; right: 12%; transform: translate(0, -50%); ">
+            <br>
+            <br>
+            <!-- <?= Html::submitButton('Update', ['class' => 'btn btn-primary']) ?> -->
+            <!-- <?= Html::a('Back', ['view'], ['class' => 'btn btn-warning']) ?> -->
+
+            <h1><?= Html::encode($this->title) ?></h1>
+
+            <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+
+            <?= $form->field($model, 'username')->textInput() ?>
+
+            <?= $form->field($model, 'email')->textInput() ?>
+
+            <?= $form->field($model, 'contactNumber')->textInput(['maxlength' => true]) ?>
+
+            <?php $hasPasswordError = $model->hasErrors('existingPassword'); ?>
+            <?= $form->field($model, 'existingPassword')->passwordInput()->hint($hasPasswordError ? '' : '   *Leave it blank if you dont want to change the password', ['style' => 'color: black']) ?>
+
+            <?php $hasPasswordError = $model->hasErrors('newPassword'); ?>
+            <?= $form->field($model, 'newPassword')->passwordInput()->hint($hasPasswordError ? '' : '   *Leave it blank if you dont want to change the password', ['style' => 'color: black']) ?>
+
+            <?= $form->field($model, 'imageFile')->fileInput(['accept' => 'image/*']) // Add the image file input field 
+            ?>
+            <?= Html::submitButton('Update', ['class' => 'btn btn-primary']) ?>
+            <?= Html::a('Back', ['view'], ['class' => 'btn btn-warning']) ?>
+
+        </div>
+        <!-- end of right  -->
     </div>
-
     <?php ActiveForm::end(); ?>
 </div>
 
@@ -83,6 +93,5 @@ $(document).ready(function() {
             reader.readAsDataURL(file);
         }
     });
-});
-");
+});");
 ?>
