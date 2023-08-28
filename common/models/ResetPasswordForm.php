c3505985665c23f6e1a302d4b35d6ad33465b380
CrownDrone <116888112+CrownDrone@users.noreply.github.com>
2023-08-28 12:17:27 +0800

    Update ResetPasswordForm.php
    password complexity okay

--- common/models/ResetPasswordForm.php
+++ common/models/ResetPasswordForm.php
@@ -18,6 +18,18 @@ class ResetPasswordForm extends Model
             [['newPassword', 'password_repeat'], 'required'],
             [['newPassword', 'password_repeat'], 'string', 'min' => 6],
             ['password_repeat', 'compare', 'compareAttribute' => 'newPassword'],
+            ['newPassword', 'validatePasswordComplexity'],
+
         ];
+    }
+
+    public function validatePasswordComplexity($attribute, $params)
+    {
+        // Regular expression to check if the password contains special characters
+        $specialCharacterRegex = '/[!@#$%^&*()\-_=+{};:,<.>]/';
+
+        if (!empty($this->newPassword) && !preg_match($specialCharacterRegex, $this->$attribute)) {
+            $this->addError($attribute, 'Password must contain special characters.');
+        }
     }
 }
