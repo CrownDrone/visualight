d4aa4c03f7c81942dba399f672137c85bd8b4cb1
CrownDrone <116888112+CrownDrone@users.noreply.github.com>
2023-11-19 18:03:59 +0800

    update send pdf design

--- backend/views/site/upload-pdf.php
+++ backend/views/site/upload-pdf.php
@@ -9,39 +9,107 @@ $this->registerJsFile('https://code.jquery.com/jquery-3.6.0.min.js', ['position'
 
 ?>
 
-<h1><?= Html::encode($this->title) ?></h1>
+<style>
+    body {
+        background-color: #f8f9fa;
+    }
 
-<?php if (Yii::$app->session->hasFlash('success')): ?>
-    <div class="alert alert-success">
-        <?= Yii::$app->session->getFlash('success') ?>
-    </div>
-<?php elseif (Yii::$app->session->hasFlash('error')): ?>
-    <div class="alert alert-danger">
-        <?= Yii::$app->session->getFlash('error') ?>
-    </div>
-<?php elseif (Yii::$app->session->hasFlash('info')): ?>
-    <div class="alert alert-info">
-        <?= Yii::$app->session->getFlash('info') ?>
-    </div>
-<?php endif; ?>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        margin-top:-70px;
+    }
 
-<div id="sending-email-message" class="alert alert-info hidden" style = "display:none;">
-        PDF attachments are sending, please wait...
+    .card {
+        max-width: 1000px;
+        width: 100%;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        border: none;
+    }
+
+    .card-header {
+        background-color: #007bff;
+        color: white;
+        text-align: center;
+        padding: 1.5rem;
+        border-radius: 10px 10px 0 0;
+    }
+
+    .card-body {
+        background-color: #ffffff;
+        padding: 2rem;
+        border-radius: 0 0 10px 10px;
+    }
+
+    .form-control-file {
+        background-color: #f8f9fa;
+        border: 1px solid #ced4da;
+        border-radius: 5px;
+        padding: 8px;
+    }
+
+    .btn-primary {
+        background-color: #007bff;
+        border: 1px solid #007bff;
+    }
+
+    .btn-primary:hover {
+        background-color: #0056b3;
+        border: 1px solid #0056b3;
+    }
+
+    .alert {
+        border-radius: 5px;
+    }
+</style>
+
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h1><?= Html::encode($this->title) ?></h1>
+        </div>
+
+        <div class="card-body">
+            <?php if (Yii::$app->session->hasFlash('success')): ?>
+                <div class="alert alert-success">
+                    <?= Yii::$app->session->getFlash('success') ?>
+                </div>
+            <?php elseif (Yii::$app->session->hasFlash('error')): ?>
+                <div class="alert alert-danger">
+                    <?= Yii::$app->session->getFlash('error') ?>
+                </div>
+            <?php elseif (Yii::$app->session->hasFlash('info')): ?>
+                <div class="alert alert-info">
+                    <?= Yii::$app->session->getFlash('info') ?>
+                </div>
+            <?php endif; ?>
+
+            <div id="sending-email-message" class="alert alert-info hidden" style ="display:none;">
+                <strong>PDF attachments are sending, please wait...</strong>
+            </div>
+
+            <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data', 'class' => 'mt-1']]); ?>
+
+            <?= $form->field($model, 'pdfFile[]')->fileInput(['multiple' => true, 'class' => 'form-control-file'])->label('Select PDF Files') ?>
+            <br>
+            <?= $form->field($model, 'selectedRoles')->checkboxList(
+                ArrayHelper::map(Yii::$app->authManager->getRoles(), 'name', 'name'),
+                [
+                    'class' => 'mt-3',
+                    'itemOptions' => ['labelOptions' => ['class' => 'normal-checkbox-label']],
+                ]
+            )->label('Select User Roles') ?>
+
+            <div class="form-group mt-4">
+                <?= Html::submitButton('Send Email', ['class' => 'btn btn-primary btn-block', 'id' => 'send-email-button']) ?>
+            </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
 </div>
-
-<?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
-
-<?= $form->field($model, 'pdfFile[]')->fileInput(['multiple' => true]) ?>
-
-<?= $form->field($model, 'selectedRoles')->checkboxList(
-    ArrayHelper::map(Yii::$app->authManager->getRoles(), 'name', 'name')
-) ?>
-
-<div class="form-group">
-    <?= Html::submitButton('Send Email', ['class' => 'btn btn-primary', 'id' => 'send-email-button']) ?>
-</div>
-
-<?php ActiveForm::end(); ?>
 
 <?php
 $js = <<< JS
